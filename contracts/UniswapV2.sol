89d117d3906ce5248bdcedf621d4443a37a26c91
haydenadams <haydenzadams@gmail.com>
2019-12-04 17:03:59 -0500

    remove structs

--- contracts/UniswapV2.sol
+++ contracts/UniswapV2.sol
@@ -14,23 +14,18 @@ contract UniswapV2 is IUniswapV2, ERC20("Uniswap V2", "UNI-V2", 18, 0), SafeTran
     using SafeMath256 for uint256;
     using UQ104x104 for uint240;
 
-    struct TokenData {
-        uint128 token0;
-        uint128 token1;
-    }
-
-    struct OracleData {
-        uint240 priceAccumulated;
-        uint16 blockNumberHalf;
-    }
-
     address public factory;
     address public token0;
     address public token1;
 
-    TokenData private reserves;
-    OracleData private oracleDataToken0;
-    OracleData private oracleDataToken1;
+    uint128 private reserveToken0;
+    uint128 private reserveToken1;
+
+    uint240 private priceAccumulatedToken0;
+    uint16 private blockNumberHalf0;
+
+    uint240 private priceAccumulatedToken1;
+    uint16 private blockNumberHalf1;
 
     bool private notEntered = true;
     modifier lock() {
@@ -80,36 +75,36 @@ contract UniswapV2 is IUniswapV2, ERC20("Uniswap V2", "UNI-V2", 18, 0), SafeTran
     }
 
     function getReserves() external view returns (uint128, uint128) {
-        return (reserves.token0, reserves.token1);
+        return (reserveToken0, reserveToken1);
     }
 
     function readOraclePricesAccumulated() external view returns (uint240, uint240) {
-        return (oracleDataToken0.priceAccumulated, oracleDataToken1.priceAccumulated);
+        return (priceAccumulatedToken0, priceAccumulatedToken1);
     }
 
     function readOracleBlockNumber() public view returns (uint32) {
-        return (uint32(oracleDataToken0.blockNumberHalf) << 16) + oracleDataToken1.blockNumberHalf;
+        return (uint32(blockNumberHalf0) << 16) + blockNumberHalf1;
     }
 
     function consultOracle() external view returns (uint240, uint240) {
         uint32 blockNumberLast = readOracleBlockNumber();
 
-        require(reserves.token0 != 0 && reserves.token1 != 0, "UniswapV2: NO_LIQUIDITY");
+        require(reserveToken0 != 0 && reserveToken1 != 0, "UniswapV2: NO_LIQUIDITY");
 
         // replicate the logic in update
         if (block.number > blockNumberLast) {
-            uint240 priceToken0 = UQ104x104.encode(reserves.token0).qdiv(reserves.token1);
-            uint240 priceToken1 = UQ104x104.encode(reserves.token1).qdiv(reserves.token0);
+            uint240 priceToken0 = UQ104x104.encode(reserveToken0).qdiv(reserveToken1);
+            uint240 priceToken1 = UQ104x104.encode(reserveToken1).qdiv(reserveToken0);
 
             uint32 blocksElapsed = block.number.downcast32() - blockNumberLast;
             return (
-                oracleDataToken0.priceAccumulated + priceToken0 * blocksElapsed,
-                oracleDataToken1.priceAccumulated + priceToken1 * blocksElapsed
+                priceAccumulatedToken0 + priceToken0 * blocksElapsed,
+                priceAccumulatedToken1 + priceToken1 * blocksElapsed
             );
         } else {
             return (
-                oracleDataToken0.priceAccumulated,
-                oracleDataToken1.priceAccumulated
+                priceAccumulatedToken0,
+                priceAccumulatedToken1
             );
         }
     }
@@ -125,125 +120,121 @@ contract UniswapV2 is IUniswapV2, ERC20("Uniswap V2", "UNI-V2", 18, 0), SafeTran
         amountOutput = (numerator / denominator).downcast128();
     }
 
-    function update(TokenData memory balances) private {
+    function update(uint128 balanceToken0, uint128 balanceToken1) private {
         uint32 blockNumberLast = readOracleBlockNumber();
 
         // if any blocks have gone by since the last time this function was called, we have to update
         if (block.number > blockNumberLast) {
             // we have to ensure that neither reserves are 0, else our price division fails
-            if (reserves.token0 != 0 && reserves.token1 != 0) {
+            if (reserveToken0 != 0 && reserveToken1 != 0) {
                 // get the prices according to the reserves as of the last official interaction with the contract
-                uint240 priceToken0 = UQ104x104.encode(reserves.token0).qdiv(reserves.token1);
-                uint240 priceToken1 = UQ104x104.encode(reserves.token1).qdiv(reserves.token0);
+                uint240 priceToken0 = UQ104x104.encode(reserveToken0).qdiv(reserveToken1);
+                uint240 priceToken1 = UQ104x104.encode(reserveToken1).qdiv(reserveToken0);
 
                 // multiply these prices by the number of elapsed blocks and add to the accumulators
                 uint32 blocksElapsed = block.number.downcast32() - blockNumberLast;
-                oracleDataToken0.priceAccumulated += priceToken0 * blocksElapsed;
-                oracleDataToken1.priceAccumulated += priceToken1 * blocksElapsed;
+                priceAccumulatedToken0 += priceToken0 * blocksElapsed;
+                priceAccumulatedToken1 += priceToken1 * blocksElapsed;
             }
 
             // update the last block number
-            oracleDataToken0.blockNumberHalf = uint16(block.number >> 16);
-            oracleDataToken1.blockNumberHalf = uint16(block.number);
+            blockNumberHalf0 = uint16(block.number >> 16);
+            blockNumberHalf1 = uint16(block.number);
         }
 
         // update reserves
-        reserves = balances;
+        reserveToken0 = balanceToken0;
+        reserveToken1 = balanceToken1;
     }
 
     function mintLiquidity(address recipient) external lock returns (uint256 liquidity) {
-        TokenData memory balances = TokenData({
-            token0: IERC20(token0).balanceOf(address(this)).downcast128(),
-            token1: IERC20(token1).balanceOf(address(this)).downcast128()
-        });
-        TokenData memory amounts = TokenData({
-            token0: balances.token0.sub(reserves.token0),
-            token1: balances.token1.sub(reserves.token1)
-        });
+        uint128 balanceToken0 = IERC20(token0).balanceOf(address(this)).downcast128();
+        uint128 balanceToken1 = IERC20(token1).balanceOf(address(this)).downcast128();
+        uint128 amountToken0 = balanceToken0.sub(reserveToken0);
+        uint128 amountToken1 = balanceToken1.sub(reserveToken1);
 
         if (totalSupply == 0) {
-            liquidity = Math.sqrt(uint256(amounts.token0).mul(amounts.token1));
+            liquidity = Math.sqrt(uint256(amountToken0).mul(amountToken1));
         } else {
             liquidity = Math.min(
-                uint256(amounts.token0).mul(totalSupply) / reserves.token0,
-                uint256(amounts.token1).mul(totalSupply) / reserves.token1
+                uint256(amountToken0).mul(totalSupply) / reserveToken0,
+                uint256(amountToken1).mul(totalSupply) / reserveToken1
             );
         }
 
         if (liquidity > 0) mint(recipient, liquidity);
-        update(balances);
+        update(balanceToken0, balanceToken1);
         emit LiquidityMinted(
-            msg.sender, recipient, amounts.token0, amounts.token1, balances.token0, balances.token1, liquidity
+            msg.sender, recipient, amountToken0, amountToken1, balanceToken0, balanceToken1, liquidity
         );
     }
 
     function burnLiquidity(address recipient) external lock returns (uint128 amountToken0, uint128 amountToken1) {
         uint256 liquidity = balanceOf[address(this)];
-        TokenData memory amounts = TokenData({
-            token0: amountToken0 = (liquidity.mul(reserves.token0) / totalSupply).downcast128(),
-            token1: amountToken1 = (liquidity.mul(reserves.token1) / totalSupply).downcast128()
-        });
-        if (amounts.token0 > 0) safeTransfer(token0, recipient, amounts.token0);
-        if (amounts.token1 > 0) safeTransfer(token1, recipient, amounts.token1);
+        amountToken0 = (liquidity.mul(reserveToken0) / totalSupply).downcast128();
+        amountToken1 = (liquidity.mul(reserveToken1) / totalSupply).downcast128();
+        if (amountToken0 > 0) safeTransfer(token0, recipient, amountToken0);
+        if (amountToken1 > 0) safeTransfer(token1, recipient, amountToken1);
         if (liquidity > 0) _burn(address(this), liquidity);
 
-        TokenData memory balances = TokenData({
-            token0: IERC20(token0).balanceOf(address(this)).downcast128(),
-            token1: IERC20(token1).balanceOf(address(this)).downcast128()
-        });
-        update(balances);
+        uint128 balanceToken0 = IERC20(token0).balanceOf(address(this)).downcast128();
+        uint128 balanceToken1 = IERC20(token1).balanceOf(address(this)).downcast128();
+
+        update(balanceToken0, balanceToken1);
         emit LiquidityBurned(
-            msg.sender, recipient, amounts.token0, amounts.token1, balances.token0, balances.token1, liquidity
+            msg.sender, recipient, amountToken0, amountToken1, balanceToken0, balanceToken1, liquidity
         );
     }
 
     function rageQuit(address output, address recipient) external lock returns (uint128 amountOutput) {
         uint256 liquidity = balanceOf[address(this)];
-        TokenData memory amounts;
+        uint128 amount0;
+        uint128 amount1;
 
         if (output == token0) {
-            amounts.token0 = amountOutput = (liquidity.mul(reserves.token0) / totalSupply).downcast128();
-            safeTransfer(token0, recipient, amounts.token0);
+            amount0 = amountOutput = (liquidity.mul(reserveToken0) / totalSupply).downcast128();
+            safeTransfer(token0, recipient, amount0);
         } else {
             require(output == token1, "UniswapV2: INVALID_OUTPUT");
-            amounts.token1 = amountOutput = (liquidity.mul(reserves.token1) / totalSupply).downcast128();
-            safeTransfer(token1, recipient, amounts.token1);
+            amount1 = amountOutput = (liquidity.mul(reserveToken1) / totalSupply).downcast128();
+            safeTransfer(token1, recipient, amount1);
         }
 
         if (liquidity > 0) _burn(address(this), liquidity);
 
-        TokenData memory balances = TokenData({
-            token0: IERC20(token0).balanceOf(address(this)).downcast128(),
-            token1: IERC20(token1).balanceOf(address(this)).downcast128()
-        });
-        update(balances);
+        uint128 balanceToken0 = IERC20(token0).balanceOf(address(this)).downcast128();
+        uint128 balanceToken1 = IERC20(token1).balanceOf(address(this)).downcast128();
+
+        update(balanceToken0, balanceToken1);
         emit LiquidityBurned(
-            msg.sender, recipient, amounts.token0, amounts.token1, balances.token0, balances.token1, liquidity
+            msg.sender, recipient, amount0, amount1, balanceToken0, balanceToken1, liquidity
         );
     }
 
     function swap(address input, address recipient) external lock returns (uint128 amountOutput) {
-        TokenData memory balances;
-        TokenData memory amounts;
+        uint128 balanceToken0;
+        uint128 balanceToken1;
+        uint128 amount0;
+        uint128 amount1;
 
         if (input == token0) {
-            balances.token0 = IERC20(input).balanceOf(address(this)).downcast128();
-            amounts.token0 = balances.token0.sub(reserves.token0);
-            amounts.token1 = amountOutput = getAmountOutput(amounts.token0, reserves.token0, reserves.token1);
-            safeTransfer(token1, recipient, amounts.token1);
-            balances.token1 = IERC20(token1).balanceOf(address(this)).downcast128();
+            balanceToken0 = IERC20(input).balanceOf(address(this)).downcast128();
+            amount0 = balanceToken0.sub(reserveToken0);
+            amount1 = amountOutput = getAmountOutput(amount0, reserveToken0, reserveToken1);
+            safeTransfer(token1, recipient, amount1);
+            balanceToken1 = IERC20(token1).balanceOf(address(this)).downcast128();
         } else {
             require(input == token1, "UniswapV2: INVALID_INPUT");
-            balances.token1 = IERC20(input).balanceOf(address(this)).downcast128();
-            amounts.token1 = balances.token1.sub(reserves.token1);
-            amounts.token0 = amountOutput = getAmountOutput(amounts.token1, reserves.token1, reserves.token0);
-            safeTransfer(token0, recipient, amounts.token0);
-            balances.token0 = IERC20(token0).balanceOf(address(this)).downcast128();
+            balanceToken1 = IERC20(input).balanceOf(address(this)).downcast128();
+            amount1 = balanceToken1.sub(reserveToken1);
+            amount0 = amountOutput = getAmountOutput(amount1, reserveToken1, reserveToken0);
+            safeTransfer(token0, recipient, amount0);
+            balanceToken0 = IERC20(token0).balanceOf(address(this)).downcast128();
         }
 
-        update(balances);
+        update(balanceToken0, balanceToken1);
         emit Swap(
-            msg.sender, recipient, amounts.token0, amounts.token1, balances.token0, balances.token1, input
+            msg.sender, recipient, amount0, amount1, balanceToken0, balanceToken1, input
         );
     }
 }
