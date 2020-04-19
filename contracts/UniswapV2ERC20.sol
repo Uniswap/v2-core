3462f93cd0d52f99739a7a79a3efd54dc69c0f6c
Moody Salem <moody.salem@gmail.com>
2020-04-18 20:55:39 -0500

    Another immutable

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -13,7 +13,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     mapping(address => uint) public override balanceOf;
     mapping(address => mapping(address => uint)) public override allowance;
 
-    bytes32 public override DOMAIN_SEPARATOR;
+    bytes32 public override immutable DOMAIN_SEPARATOR;
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant override PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint) public override nonces;
