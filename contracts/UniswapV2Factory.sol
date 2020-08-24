54959f373466958b2806fa12a98b8dac118bec3c
William Entriken <github.com@phor.net>
2020-08-24 12:14:59 -0400

    Remove duplicative pair storage

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -31,8 +31,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
         IUniswapV2Pair(pair).initialize(token0, token1);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        getPair[token0][token1] = pair; // single storage is sufficient
         allPairs.push(pair);
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
