94f3fc2b9f119ed5083593a81f224373566f81d0
Semen Medvedev <smedvedev@golos.io>
2021-03-22 12:03:46 +0700

    Fix invalid token0/token1 assignment in Pair fixture

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -38,7 +38,7 @@ export async function pairFixture(provider:JsonRpcProvider, [wallet]: Wallet[]):
   const pairAddress = await factory.getPair(tokenA.address, tokenB.address)
   const pair = new Contract(pairAddress, JSON.stringify(UniswapV2Pair.abi), provider).connect(wallet)
 
-  const token0Address = (await pair.token0()).address
+  const token0Address = (await pair.token0())
   const token0 = tokenA.address === token0Address ? tokenA : tokenB
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
