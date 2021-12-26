c8fe3d94cb1cd4017487e2a046ca24f9d684c5e6
Adam Schmid <adam.genesis.pro@gmail.com>
2021-12-26 17:20:44 -0300

    Updated lp token name and symbol

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -6,8 +6,8 @@ import './libraries/SafeMath.sol';
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Uniswap V2';
-    string public constant symbol = 'UNI-V2';
+    string public constant name = 'Genesis LP';
+    string public constant symbol = 'GEN-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
