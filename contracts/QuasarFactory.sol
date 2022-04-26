170a8eb9d1ad529f07f9fe082a3cbb31bfa5924b
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-27 00:31:37 +0100

    add custom math library

--- contracts/QuasarFactory.sol
+++ contracts/QuasarFactory.sol
@@ -11,8 +11,6 @@ contract QuasarFactory is IQuasarFactory, Ownable {
   mapping(address => mapping(address => address)) public getPair;
   address[] public allPairs;
 
-  event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
-
   constructor(address _feeToSetter) public {
     feeToSetter = _feeToSetter;
   }
@@ -26,12 +24,12 @@ contract QuasarFactory is IQuasarFactory, Ownable {
     (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
     require(token0 != address(0), 'Quasar: ZERO_ADDRESS');
     require(getPair[token0][token1] == address(0), 'Quasar: PAIR_EXISTS'); // single check is sufficient
-    bytes memory bytecode = type(UniswapV2Pair).creationCode;
+    bytes memory bytecode = type(QuasarPair).creationCode;
     bytes32 salt = keccak256(abi.encodePacked(token0, token1));
     assembly {
       pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
     }
-    IUniswapV2Pair(pair).initialize(token0, token1);
+    IQuasarPair(pair).initialize(token0, token1);
     getPair[token0][token1] = pair;
     getPair[token1][token0] = pair; // populate mapping in the reverse direction
     allPairs.push(pair);
