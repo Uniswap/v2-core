ba0ad683131b94b967182b0c078c3870891b6f16
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-26 23:26:42 +0100

    add migrations file

---
+++ contracts/Migrations.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+  address public owner = msg.sender;
+  uint256 public last_completed_migration;
+
+  modifier restricted() {
+    require(msg.sender == owner, "This function is restricted to the contract's owner");
+    _;
+  }
+
+  function setCompleted(uint256 completed) public restricted {
+    last_completed_migration = completed;
+  }
+}
