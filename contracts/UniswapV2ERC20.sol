7b9ce274602d34b5c43e95f6b6f951d498b06057
l303l <l303l@l303ls-MacBook-Air.local>
2022-05-02 21:23:58 -0400

    pass test

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -11,7 +11,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
     string public constant name = 'ETC-SWAP';
-    string public constant symbol = 'ETC_SWAP';
+    string public constant symbol = 'ETC-SWAP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
