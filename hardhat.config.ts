4304b498aeb19528a0c8d9d772aeafffa97c8820
inaridiy <inainainariinainari@gmail.com>
2022-05-06 23:22:16 +0900

    hardhatの環境を構築

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+    ],
+  },
+};
