b3c0834578c7205bbe61dd748fc77b0f11355691
ronin <58588775+gitr0n1n@users.noreply.github.com>
2022-05-06 18:50:49 -0600

    Update UniswapV2ERC20.sol
    Fix format of "name" field

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -10,7 +10,7 @@ import './libraries/SafeMath.sol';
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'ETC-SWAP-V2';
+    string public constant name = 'ETC Swap V2';
     string public constant symbol = 'ETC-SWAP-V2';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
