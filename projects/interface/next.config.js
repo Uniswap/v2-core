89106ccce3daba4ad5a9f4eea1200df34824517a
inaridiy <inainainariinainari@gmail.com>
2022-05-10 19:39:20 +0900

    リダイレクトを設定

--- projects/interface/next.config.js
+++ projects/interface/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/", // リダイレクト元のURL
+        destination: "/swap", // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
