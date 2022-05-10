8035bc38529c88dc03ba414ee93130c62d31589d
inaridiy <inainainariinainari@gmail.com>
2022-05-11 00:39:20 +0900

    Swapの側だけ作成

--- projects/interface/src/components/Pages/Swap.tsx
+++ projects/interface/src/components/Pages/Swap.tsx
@@ -1,13 +1,33 @@
-import { PageContainer, Card } from "../ui";
+import { Card, PageContainer } from "../ui";
+
+const CurrencyInput: React.VFC<
+  { symbol: string } & JSX.IntrinsicElements["input"]
+> = ({ symbol, ...props }) => {
+  return (
+    <div className="input-group input-group-lg">
+      <input
+        type="text"
+        placeholder="0.0"
+        className="text-2xl font-bold input input-lg input-bordered"
+        {...{ props }}
+      />
+      <div className="grow btn btn-lg">{symbol}</div>
+    </div>
+  );
+};
 
 const Swap: React.VFC = () => {
   return (
     <PageContainer>
-      <Card className="w-96 h-64">
+      <Card>
         <div className="card-body">
-          <h2 className="card-title text-xl">Swap</h2>
-          <input type="text" />
-          <input type="text" />
+          <h2 className="text-xl card-title">Swap</h2>
+          <CurrencyInput symbol="ASTR" />
+          <CurrencyInput symbol="INR" />
+          <div className="card-actions">
+            <button className="btn">Approve</button>
+            <button className="btn">Add Liquidity</button>
+          </div>
         </div>
       </Card>
     </PageContainer>
