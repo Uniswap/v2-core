b680c93855faa081b1f2014c346e7358c81118d4
inaridiy <inainainariinainari@gmail.com>
2022-05-18 00:51:55 +0900

    logoのパスの設定をついか

--- projects/interface/src/constants/logos.ts
+++ projects/interface/src/constants/logos.ts
@@ -1,3 +1,11 @@
+const p = (name: string) => `/logo/${name}`;
+
 export const logos = {
-  "0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720": "",
+  "0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720": p("astar-logo.png"),
+  "0x81ECac0D6Be0550A00FF064a4f9dd2400585FE9c": p("ethereum-eth-logo.svg"),
+  "0xad543f18cFf85c77E140E3E5E3c3392f6Ba9d5CA": p("bitcoin-btc-logo.svg"),
+  "0x75364D4F779d0Bd0facD9a218c67f87dD9Aff3b4": p("shiden-logo.png"),
+  "0x6a2d262D56735DbA19Dd70682B39F6bE9a931D98": p("usd-coin-usdc-logo.svg"),
+  "0x3795C36e7D12A8c252A20C5a7B455f7c57b60283": p("tether-usdt-logo.svg"),
+  "0x7f27352D5F83Db87a5A3E00f4B07Cc2138D8ee52": p("bnb-bnb-logo.svg"),
 };
