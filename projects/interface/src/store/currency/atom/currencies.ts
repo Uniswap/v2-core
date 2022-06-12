cd6fe5f29f129d5277033b097874357e4729ced7
inaridiy <inainainariinainari@gmail.com>
2022-06-12 18:22:24 +0900

    currenciesにNative Currencyも追加

--- projects/interface/src/store/currency/atom/currencies.ts
+++ projects/interface/src/store/currency/atom/currencies.ts
@@ -1,9 +1,10 @@
 import { chains } from "@/constants/chains";
 import { chainTokens } from "@/constants/tokens";
+import { Currency } from "@penta-swap/sdk";
 import { atomFamily } from "recoil";
 import { Currencies } from "../types";
 
 export const currenciesState = atomFamily<Currencies, chains>({
   key: "currencies",
-  default: (chain) => chainTokens[chain],
+  default: (chain) => [Currency.ETHER, ...chainTokens[chain]],
 });
