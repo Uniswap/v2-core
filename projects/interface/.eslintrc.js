6e4a73c7eee4b1774417eeb59099cfbb66ae4199
inaridiy <inainainariinainari@gmail.com>
2022-06-25 09:45:26 +0900

    設定を調整

--- projects/interface/.eslintrc.js
+++ projects/interface/.eslintrc.js
@@ -33,4 +33,5 @@ module.exports = {
       },
     ],
   },
+  include: ["src"],
 };
--- projects/interface/vite.config.ts
+++ projects/interface/vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": `${__dirname}/src/`,
+    },
+  },
 });
