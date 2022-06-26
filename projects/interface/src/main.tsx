885248a23d29c088406abebe4279b7232c2825fb
inaridiy <inainainariinainari@gmail.com>
2022-06-26 14:06:09 +0900

    tailwindcssを導入

--- projects/interface/src/main.tsx
+++ projects/interface/src/main.tsx
@@ -2,6 +2,7 @@ import invariant from "@/utils/invariant";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./main.css";
 
 const root = document.getElementById("root");
 invariant(root);
