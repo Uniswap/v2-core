f4fc4f52f004451c783739c58a604f00de41a217
inaridiy <inainainariinainari@gmail.com>
2022-06-27 23:20:50 +0900

    最低限のLoadingを実装

--- projects/interface/src/providers/fallback.tsx
+++ projects/interface/src/providers/fallback.tsx
@@ -1,7 +1,13 @@
+import { Spinner } from "@/components/Elements";
+
 export const ErrorFallBack = () => {
   return <div></div>;
 };
 
 export const LoadingFallBack = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Spinner size="lg" />
+    </div>
+  );
 };
