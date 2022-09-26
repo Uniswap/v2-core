4ff791b7b077bd29e3492144616ce3bf8e777f7e
Kingsley Victor <javaprodigy56@gmail.com>
2022-09-26 22:07:57 +0100

    ...

--- contracts/interfaces/IQuasarPair.sol
+++ contracts/interfaces/IQuasarPair.sol
@@ -3,14 +3,7 @@ pragma solidity ^0.8.0;
 interface IQuasarPair {
   event Mint(address indexed sender, uint256 amount0, uint256 amount1);
   event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
-  event Swap(
-    address indexed sender,
-    uint256 amount0In,
-    uint256 amount1In,
-    uint256 amount0Out,
-    uint256 amount1Out,
-    address indexed to
-  );
+  event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to);
   event Sync(uint112 reserve0, uint112 reserve1);
 
   function MINIMUM_LIQUIDITY() external pure returns (uint256);
