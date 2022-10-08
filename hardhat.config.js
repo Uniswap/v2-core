e6017e8989fc7df5e719b1c2180e58bb21bce067
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-08 11:52:49 +0100

    deploy to smartchain testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,8 @@ require('@nomiclabs/hardhat-waffle');
 // require('@nomiclabs/hardhat-truffle5');
 require('@nomiclabs/hardhat-etherscan');
 
+require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.17',
@@ -13,5 +15,15 @@ module.exports = {
       accounts: ['0x9bce709a035954deb674a4538ac91cf90518777c98d608c008a31ef700814ffd'], // Try stealing the funds in this
       chainId: 1337,
     },
+    bsc_testnet: {
+      url: 'https://bsctestapi.terminet.io/rpc',
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 97
+    }
   },
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.BSC_API_KEY
+    }
+  }
 };
