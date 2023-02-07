572df5bb452c8945f1e2a2ea76603b6eb8a3e0f4
David Adamyan <d.adamyan@ongrid.pro>
2023-02-07 15:37:06 +0400

    deploy all contracts with csutom contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,6 +19,15 @@ module.exports = {
             runs: 200
           }
         }
+      },
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       }
     ]
   },
