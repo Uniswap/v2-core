7e695379647b728c9268c14030f1a89af077cd34
Chris Gorman <chgorman@users.noreply.github.com>
2023-08-31 18:48:05 -0700

    Minor improvement.

--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -13,14 +13,14 @@ function sqrt(uint256 x) internal pure returns (uint256) {
         if (x >= ((1 << 128) - 1)**2) { return (1 << 128) - 1; }
         uint256 xAux = x;
         uint256 result = 1;
-        if (xAux >= (1 << 128)) { xAux >>= 128; result <<= 64; }
+        if (xAux >= (1 << 128)) { xAux >>= 128; result = 1 << 64; }
         if (xAux >= (1 << 64 )) { xAux >>= 64;  result <<= 32; }
         if (xAux >= (1 << 32 )) { xAux >>= 32;  result <<= 16; }
         if (xAux >= (1 << 16 )) { xAux >>= 16;  result <<= 8;  }
         if (xAux >= (1 << 8  )) { xAux >>= 8;   result <<= 4;  }
         if (xAux >= (1 << 4  )) { xAux >>= 4;   result <<= 2;  }
         if (xAux >= (1 << 2  )) {               result <<= 1;  }
-        result += (result >> 1);
+        result = (3 * result) >> 1;
         result = (result + x / result) >> 1;
         result = (result + x / result) >> 1;
         result = (result + x / result) >> 1;
