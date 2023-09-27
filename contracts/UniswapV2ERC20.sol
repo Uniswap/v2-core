eabce366108d768fee0de5232c86f15e07a1757f
Steven Thornton <steven.thornton@openzeppelin.com>
2023-09-27 10:05:22 -0400

    Fix stuff

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -8,7 +8,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
 
     string public constant name = 'Uniswap V2';
     string public constant symbol = 'UNI-V2';
-    uint8 public constant decimals = 18;
+    uint8 public constant decimals_ = 3;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
