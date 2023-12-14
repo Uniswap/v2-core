dfb3e5f44ce032dc07f12a5874458267034ae302
OzoemenaIke <93989793+OzoemenaIke@users.noreply.github.com>
2023-12-14 18:56:14 +0100

    new (working)

--- interactWithERC20.js
+++ interactWithERC20.js
@@ -11,12 +11,17 @@ module.exports = async function(callback) {
         // Display original balances
         let balance0 = await erc20.balanceOf(accounts[0]);
         let balance1 = await erc20.balanceOf(accounts[1]);
+        console.log("Account 0: ", accounts[0]);
+        console.log("Account 1: ", accounts[1]);
         console.log("Original Balance of account 0: ", balance0.toString());
         console.log("Original Balance of account 1: ", balance1.toString());
 
         // Transfer tokens
         let amount = web3.utils.toWei('10', 'ether'); // for example, transferring 10 tokens
-        await erc20.transfer(accounts[1], amount, { from: accounts[0] });
+        const transferResult =  await erc20.transfer(accounts[1], amount, { from: accounts[0] });
+
+        // Print the transaction hash
+        console.log("Transaction hash: ", transferResult.tx);
 
         // Display new balances
         balance0 = await erc20.balanceOf(accounts[0]);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,12 @@ const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 const UniswapV2ERC20 = artifacts.require("UniswapV2ERC20");
 const UniswapV2ERC20Test = artifacts.require("UniswapV2ERC20Test"); // Add this line
 
+
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(UniswapV2ERC20)
     .then(() => deployer.deploy(UniswapV2ERC20Test)) // Deploy the test contract
-    .then(() => deployer.deploy(UniswapV2Factory, accounts[0])); // accounts[0] as the fee setter
+    .then(() => deployer.deploy(UniswapV2Factory, accounts[0])) // accounts[0] as the fee setter
+    .then(() => {console.log("UniswapV2ERC20Test deployed at address:", UniswapV2ERC20Test.address)})
+    .then(() => {console.log("UniswapV2ERC20 deployed at address:", UniswapV2ERC20.address)})
+    .then(() => {console.log("UniswapV2Factory deployed at address:", UniswapV2Factory.address)});
 };
